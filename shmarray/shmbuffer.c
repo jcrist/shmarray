d8d859b50260cacb69b3953d542db402dff0a12a
Jim Crist <jiminy.crist@gmail.com>
2017-08-11 12:18:02 -0500

    Support refcounting

--- shmarray/shmbuffer.c
+++ shmarray/shmbuffer.c
@@ -2,6 +2,7 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdatomic.h>
 
 #include <Python.h>
 #include "structmember.h"
@@ -26,12 +27,56 @@ typedef struct {
 } shmbuffer_object;
 
 
+typedef struct {
+    atomic_uint refcount;
+    size_t size;
+} shmbuffer_header;
+
+
+static void
+shmbuffer_init(char *buffer, size_t size) {
+    shmbuffer_header *header = (shmbuffer_header *)buffer;
+    header->refcount = ATOMIC_VAR_INIT(1);
+    header->size = size;
+}
+
+
+static unsigned int
+shmbuffer_incref(char *buffer) {
+    shmbuffer_header *header = (shmbuffer_header *)buffer;
+    return atomic_fetch_add(&(header->refcount), 1) + 1;
+}
+
+
+static unsigned int
+shmbuffer_decref(char *buffer) {
+    shmbuffer_header *header = (shmbuffer_header *)buffer;
+    return atomic_fetch_sub(&(header->refcount), 1) - 1;
+}
+
+
+static unsigned int
+shmbuffer_getref(char *buffer) {
+    shmbuffer_header *header = (shmbuffer_header *)buffer;
+    return atomic_load(&(header->refcount));
+}
+
+
+static size_t
+shmbuffer_getsize(char *buffer) {
+    shmbuffer_header *header = (shmbuffer_header *)buffer;
+    return header->size;
+}
+
+
 static void
 shmbuffer_object_dealloc(shmbuffer_object *self)
 {
     if (self->data != NULL) {
+        if (!shmbuffer_decref(self->data)) {
+            shm_unlink(self->name);
+        }
         munmap(self->data, self->size);
-        shm_unlink(self->name);
     }
 
     if (self->weakreflist != NULL)
@@ -54,8 +99,10 @@ shmbuffer_close_method(shmbuffer_object *self, PyObject *unused)
     }
 
     if (self->data != NULL) {
+        if (!shmbuffer_decref(self->data)) {
+            shm_unlink(self->name);
+        }
         munmap(self->data, self->size);
-        shm_unlink(self->name);
         self->data = NULL;
     }
 
@@ -79,6 +126,20 @@ shmbuffer_closed_get(shmbuffer_object *self)
     return PyBool_FromLong(self->data == NULL ? 1 : 0);
 }
 
+static PyObject *
+shmbuffer_refcount_get(shmbuffer_object *self)
+{
+    CHECK_VALID(NULL);
+    return PyLong_FromUnsignedLong(shmbuffer_getref(self->data));
+}
+
+static PyObject *
+shmbuffer_size_get(shmbuffer_object *self)
+{
+    CHECK_VALID(NULL);
+    return PyLong_FromSize_t(self->size);
+}
+
 static struct PyMethodDef shmbuffer_object_methods[] = {
     {"close",     (PyCFunction) shmbuffer_close_method,   METH_NOARGS},
     {NULL, NULL}  /* sentinel */
@@ -86,6 +147,8 @@ static struct PyMethodDef shmbuffer_object_methods[] = {
 
 static PyGetSetDef shmbuffer_object_getset[] = {
     {"closed", (getter) shmbuffer_closed_get, NULL, NULL},
+    {"refcount", (getter) shmbuffer_refcount_get, NULL, NULL},
+    {"size", (getter) shmbuffer_size_get, NULL, NULL},
     {NULL}  /* sentinel */
 };
 
@@ -94,7 +157,9 @@ static int
 shmbuffer_buffer_getbuf(shmbuffer_object *self, Py_buffer *view, int flags)
 {
     CHECK_VALID(-1);
-    if (PyBuffer_FillInfo(view, (PyObject*)self, self->data, self->size,
+    if (PyBuffer_FillInfo(view, (PyObject*)self,
+                          self->data + sizeof(shmbuffer_header),
+                          self->size - sizeof(shmbuffer_header),
                           (self->mode == MODE_READ), flags) < 0)
         return -1;
     self->exports++;
@@ -167,6 +232,9 @@ new_shmbuffer_object(PyTypeObject *type, PyObject *args, PyObject *kwdict)
     if (fd < 0)
         return PyErr_SetFromErrnoWithFilename(PyExc_OSError, name);
 
+    /* Include the header in the size */
+    map_size += sizeof(shmbuffer_header);
+
     /* Grow the file if needed */
     if (resize) {
         if (ftruncate(fd, map_size) < 0) {
@@ -178,14 +246,24 @@ new_shmbuffer_object(PyTypeObject *type, PyObject *args, PyObject *kwdict)
 
     /* Map it */
     map_addr = mmap(NULL, map_size, prot, MAP_SHARED, fd, 0);
-    close(fd);
     if (map_addr == MAP_FAILED) {
+        close(fd);
         shm_unlink(name);
         return PyErr_SetFromErrnoWithFilename(PyExc_OSError, name);
     }
 
+    /* Initialize if needed */
+    if (resize) {
+        shmbuffer_init(map_addr, map_size);
+    }
+    else {
+        shmbuffer_incref(map_addr);
+    }
+    close(fd);
+
     self = (shmbuffer_object *)type->tp_alloc(type, 0);
     if (self == NULL) {
+        shmbuffer_decref(map_addr);
         munmap(map_addr, map_size);
         shm_unlink(name);
         return NULL;
